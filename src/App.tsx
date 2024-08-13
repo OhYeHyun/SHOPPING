5647b7df9edfb4deb412461de0d79caf3e7b29cf
OhYeHyun <ohyhohyl@naver.com>
2024-08-13 17:47:28 +0900

    삭제 버튼 css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./assets/style.css";
 import {useEffect, useRef, useState} from "react";
 
 const itemData = [
-    "우유",
-    "라면",
+  "우유",
+  "라면",
 ]
 // localStorage.removeItem('items')
 
@@ -19,9 +19,12 @@ function App() {
   const removeRefs = useRef<(HTMLButtonElement | null)[]>([])
 
   const handleRemove = (idx: number) => {
-    alert(`"${items[idx]}"이(가) 삭제되었습니다.`)
     const updatedItems = items.filter((_: any, i: number) => idx !== i)
     setItems(updatedItems)
+  }
+
+  const handleCheck = (idx: number) => {
+    const listToCheck = items[idx];
   }
 
   useEffect(() => {
@@ -29,53 +32,55 @@ function App() {
   }, [items]);
 
   return (
-      <>
-        <div className="outer">
-          <div className="shopping">shopping</div>
+    <>
+      <div className="outer">
+        <div className="shopping">shopping</div>
 
-          <ul className="all-list">
-            {items.map((item: string, idx: number) => (
-              <li key={idx} className="custom-li">
-                <button className="check custom-button">
-                  <i className="fa fa-circle-check"></i>
+        <ul className="all-list">
+          {items.map((item: string, idx: number) => (
+            <li key={idx} className="custom-li">
+              <button className="check custom-button">
+                <i className="fa fa-circle-check"></i>
+              </button>
+              <div className="title shop-item">
+                <span>{item}</span>
+                <button
+                  className="delete"
+                  ref={(el) => (removeRefs.current[idx] = el)}
+                  onClick={() => handleRemove(idx)}
+                >
+                  <i className="fa fa-trash"></i>
                 </button>
-                <div className="title shop-item">
-                  <span>{item}</span>
-                  <button
-                    className="delete"
-                    ref={(el) => (removeRefs.current[idx] = el)}
-                    onClick={() => handleRemove(idx)}
-                  >
-                    <i className="fa fa-trash"></i>
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
 
-          <div className="write">
-            <input
-                ref={inputRef}
-                style={{
-                  height: "60%",
-                  fontSize: "2rem",
-                  width: "100%",
-                  borderRadius: "0.5rem",
-                  border: "none"
-            }} />
+        <div className="write">
+          <input
+            ref={inputRef}
+            style={{
+              height: "60%",
+              fontSize: "2rem",
+              width: "100%",
+              borderRadius: "0.5rem",
+              border: "none"
+            }}
+            placeholder='아이디를 입력하세요.'
+            autoFocus={true}
+          />
 
-            <div className="item-add-button"
-                 onClick={() => {
-                   const item = inputRef.current!.value;
-                   inputRef.current!.value = "";
-                   setItems([...items, item]);
-                   alert(`"${item}"이(가) 추가되었습니다.`)
-                 }}>
-              <i className="fa fa-square-plus"></i>
-            </div>
+          <div className="item-add-button"
+            onClick={() => {
+              const item = inputRef.current!.value;
+              inputRef.current!.value = "";
+              setItems([...items, item]);
+            }}>
+            <i className="fa fa-square-plus"></i>
           </div>
-        </div>
-      </>
+         </div>
+      </div>
+    </>
   );
 }
 
